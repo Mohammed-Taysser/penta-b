6d330d707a524ed1cd73bf026107cd742411eeb6
mohammed-taysser <mohamedtaysser983@gmail.com>
2023-09-03 14:35:11 +0300

    add app typescript types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,33 @@
 /// <reference types="vite/client" />
+
+interface RoverProps {
+  ghost?: boolean;
+  direction: string;
+}
+
+type Position = [number, number, Direction];
+
+type Command = 'F' | 'D' | 'R' | 'L';
+
+type Direction = 'NORTH' | 'EAST' | 'WEST' | 'SOUTH';
+
+type Vector = { [key in Direction]: number[] };
+
+interface MarsProps {
+  size: number;
+  position: Position;
+  commands: string;
+  start: boolean;
+  reset: boolean;
+  onDone: () => void;
+}
+
+interface MarsInitState {
+  start: string;
+  end: string;
+  position: string;
+  direction: Direction;
+  path: { [key: string]: string };
+  error: boolean;
+  commandIndex: number;
+}
